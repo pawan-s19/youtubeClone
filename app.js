85f43750d3644fd6f03b2bb1bc85aa448ec0f50d
Pawan <pawans6343@gmail.com>
2023-03-07 12:26:40 +0530

    changed to cookie session instead of express session

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-var expressSession = require("express-session");
+var expressSession = require("cookie-session");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./models/userModel");
