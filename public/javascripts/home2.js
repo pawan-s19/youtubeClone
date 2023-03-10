e53b1ef977b8a8f54d52f239b0ed65d3950c3a24
tjain-web-developer <tanishqjain2114@gmail.com>
2023-03-10 19:02:50 +0530

    created bottom menu, added voice recognition and setup needed partial ejs pages

--- public/javascripts/home2.js
+++ public/javascripts/home2.js
@@ -1,96 +1,3 @@
-let Searchinp = document.querySelectorAll('.nav-searchbox form input');
-let Searchbtn = document.querySelectorAll('.nav-searchbox form button');
-// let micBtn = document.querySelectorAll('.mic');
-
-// Text Recognition
-const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-
-// if(SpeechRecognition){
-//     console.log('Browser Supports Speech Recognition')
-//     let bigmicBtn = document.querySelector('.big-mic');
-//     let smallmicBtn = document.querySelector('.small-mic');
-
-//     bigmicBtn.addEventListener('click', micBtnClick);
-//     smallmicBtn.addEventListener('click', micBtnClick);
-
-//     const recognition = new SpeechRecognition();
-//     let currentMic;
-//     function micBtnClick(){
-//         currentMic = this;
-//         if(this.classList.contains('ri-mic-fill')){
-//             recognition.start();
-//         }else{
-//             recognition.stop();
-//         }
-//     }
-
-//     recognition.addEventListener('start', function(){
-//         currentMic.classList.remove("ri-mic-off-fill");
-//         currentMic.classList.add("ri-mic-fill");
-//         console.log('speech recognition active');
-//     });
-//     recognition.addEventListener('end', function(){
-//         console.log(currentMic);
-//         currentMic.classList.remove("ri-mic-fill");
-//         currentMic.classList.add("ri-mic-off-fill");
-//         console.log('speech recognition disconnected');
-//     });
-// }else{
-//     console.log('Browser Do Not Supports Speech Recognition')
-// }
-
-if(SpeechRecognition){
-    console.log('Browser Supports Speech Recognition')
-    let bigmicBtn = document.querySelector('.big-mic');
-    let smallmicBtn = document.querySelector('.small-mic');
-
-    const recognition = new SpeechRecognition();
-    recognition.interimResults = true;
-    recognition.continuous = true;
-
-    bigmicBtn.addEventListener('click', micBtnClick);
-    smallmicBtn.addEventListener('click', micBtnClick);
-
-    function micBtnClick(){
-    // let currentMic;
-        // currentMic = this;
-        if(bigmicBtn.classList.contains("ri-mic-fill")){
-            bigmicBtn.classList.add("ri-mic-off-fill");
-            bigmicBtn.classList.remove("ri-mic-fill");
-            recognition.start();
-        }else{
-            console.log('hello')
-            bigmicBtn.classList.add("ri-mic-fill");
-            bigmicBtn.classList.remove("ri-mic-off-fill");
-            recognition.stop();
-        }
-    }
-
-    recognition.addEventListener('start', function(){
-        console.log('speech recognition active');
-    });
-    recognition.addEventListener("result", function(event){
-        const transcript = event.results[0][0].transcript;
-        console.log(transcript)
-        console.log(event)
-    })
-}else{
-    console.log('Browser Do Not Supports Speech Recognition')
-}
-
-Searchinp.forEach((inp) => {
-    inp.addEventListener('click', () =>{
-        console.log('hey')
-    })
-    inp.addEventListener('input', function(){
-        if(this.value.length > 0){
-            this.nextSibling.nextSibling.disabled = false;
-        }else{
-            this.nextSibling.nextSibling.disabled = true;
-        }
-    })
-})
-
 window.addEventListener("online", function() {
   console.log("I am connected to the internet")
 })
@@ -99,40 +6,39 @@ window.addEventListener("offline", function() {
   console.log("Disconnected...so sad!!!")
 })
 
-// inp.addEventListener('input', (e) => {
-//     if(e.target.value.length > 0){
-//         Searchbtn.disabled = false;
-//     }else{
-//         Searchbtn.disabled = true;
-//     }
-// })
+let allVideosCtn = document.querySelector('.all-videos-ctn');
+let totalVideos = allVideosCtn.childElementCount;
 
-let openSearchBtn = document.querySelector('.OpenSearch');
-let closeSearchBtn = document.querySelector('.CloseSearch');
-let smNavBar = document.querySelector('.small-screen-search');
+let setGapBetween = () => {
+    let lastRowVideoCount = totalVideos % 4;
 
-// Utility Functions
-let hideSmallScreenSearchBox = () => {
-    smNavBar.style.top = '-60px';
-    smNavBar.style.opacity = 0;
+    if(allVideosCtn.childElementCount > 4 && (totalVideos % 4 !== 0)){
+        let firstBox = allVideosCtn.children[0].offsetLeft + allVideosCtn.children[0].getBoundingClientRect().width;
+        let secondBox = allVideosCtn.children[1].offsetLeft
+        let gap = secondBox - firstBox;
+
+        console.log(lastRowVideoCount)
+        let lastElem = allVideosCtn.children[totalVideos - 1];
+        let SecondlastElem = allVideosCtn.children[totalVideos - 2];
+        let ThirdlastElem = allVideosCtn.children[totalVideos - 3];
+
+        console.log(lastElem, SecondlastElem, ThirdlastElem)
+
+        // lastElem && lastElem.style.marginRight = `${gap}px`;
+        // SecondlastElem && SecondlastElem.style.marginRight = `${gap}px`;
+        // ThirdlastElem && ThirdlastElem.style.marginRight = `${gap}px`;
+
+        console.log(gap);
+        // allVideosCtn.style.setProperty("--spaceBetween", `${gap}px`);
+    }
 }
 
-openSearchBtn.addEventListener('click', () => {
-    smNavBar.style.top = '0';
-    smNavBar.style.opacity = 1;
-});
-closeSearchBtn.addEventListener('click', () => {
-    hideSmallScreenSearchBox();
-});
+// console.log(allVideosCtn.childElementCount)
+// console.log(allVideosCtn.children)
+// console.log(allVideosCtn.childNodes)
 
+// setGapBetween();
 
-function myFunction(x) {
-  if (x.matches) { // If media query matches
-  } else {
-    hideSmallScreenSearchBox();
-  }
-}
-
-var x = window.matchMedia("(max-width: 630px)")
-myFunction(x) // Call listener function at run time
-x.addListener(myFunction) // Attach listener function on state changes
+window.addEventListener('resize', function(event) {
+    // setGapBetween();
+}, true);
---
+++ public/javascripts/navbar.js
@@ -0,0 +1,116 @@
+let Searchinp = document.querySelectorAll('.nav-searchbox form input');
+let Searchbtn = document.querySelectorAll('.nav-searchbox form button');
+let micBtn = document.querySelectorAll('.mic');
+
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+if(SpeechRecognition){
+    console.log('Support');
+    const recognition = new SpeechRecognition();
+    recognition.continuous = true;
+    recognition.interimResults = true;
+
+    let currentMic;
+    micBtn.forEach(e=>{
+        e.addEventListener('click', function(){
+            currentMic = this;
+            if(this.classList.contains("ri-mic-fill")){
+                currentMic.classList.add("ri-mic-off-fill");
+                currentMic.classList.remove("ri-mic-fill");
+                currentMic.style.setProperty('--anim', "scale-up-center 1s cubic-bezier(0.390, 0.575, 0.565, 1.000) infinite alternate-reverse");
+                currentMic.style.setProperty('--displayMicEffect', "1");
+                recognition.start();
+            }else{
+                runOnMicOff();
+                recognition.stop();
+            }
+        });
+    });
+
+    recognition.addEventListener("start", function(){
+        focusOnInput();
+    });
+
+    recognition.addEventListener("end", function(){
+        runOnMicOff();
+        focusOnInput();
+    })
+
+    recognition.addEventListener("result", function(event){
+        const currentIndex = event.resultIndex;
+        const transcript = event.results[currentIndex][0].transcript;
+        addText(transcript);
+    })
+
+    // focusOnInput
+    function focusOnInput(){
+        Searchinp.forEach(e=>{
+            e.focus();
+        })
+    }
+
+    // AddTextToInput
+    function addText(trallVideosCtncript){
+        Searchinp.forEach(e=>{
+            if(trallVideosCtncript.length > 0){
+                e.value = trallVideosCtncript;
+                e.nextSibling.nextSibling.disabled = false;
+            }
+        })
+    }
+
+    //RunEventsOnMicOff
+    function runOnMicOff(){
+        currentMic.classList.add("ri-mic-fill");
+        currentMic.classList.remove("ri-mic-off-fill");
+        currentMic.style.setProperty('--anim', "0");
+        currentMic.style.setProperty('--displayMicEffect', "0");
+    }
+
+}else{
+    console.log('No Support');
+}
+
+Searchinp.forEach((inp) => {
+    inp.addEventListener('click', () =>{
+        console.log('hey')
+    })
+    inp.addEventListener('input', function(){
+        if(this.value.length > 0){
+            this.nextSibling.nextSibling.disabled = false;
+        }else{
+            this.nextSibling.nextSibling.disabled = true;
+        }
+    })
+})
+
+
+let openSearchBtn = document.querySelector('.OpenSearch');
+let closeSearchBtn = document.querySelector('.CloseSearch');
+let smNavBar = document.querySelector('.small-screen-search');
+
+// Utility Functions
+let hideSmallScreenSearchBox = () => {
+    smNavBar.style.top = '-60px';
+    smNavBar.style.opacity = 0;
+}
+
+openSearchBtn.addEventListener('click', () => {
+    smNavBar.style.top = '0';
+    smNavBar.style.opacity = 1;
+});
+closeSearchBtn.addEventListener('click', () => {
+    hideSmallScreenSearchBox();
+});
+
+
+function myFunction(x) {
+    if (x.matches) { // If media query matches
+    } else {
+      hideSmallScreenSearchBox();
+    }
+  }
+  
+  var x = window.matchMedia("(max-width: 630px)")
+  myFunction(x) // Call listener function at run time
+  x.addListener(myFunction) // Attach listener function on state changes
